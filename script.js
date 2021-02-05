69e87d8ed4db1799a71f8b35cd99de1ac0abe235
icegreen <flavioaquila16@gmail.com>
2021-02-05 09:00:33 -0300

    :package:

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+  
+    document.querySelector("#qtde").addEventListener("change", atualizarPreco);
+    document.querySelector("#js").addEventListener("change", atualizarPreco);
+    document.querySelector("#layout-sim").addEventListener("change", atualizarPreco);
+    document.querySelector("#layout-nao").addEventListener("change", atualizarPreco);
+    
+    document.querySelector("#prazo").addEventListener("change", function () {
+        const prazo = document.querySelector("#prazo").value
+        document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+        atualizarPreco()
+    })
 
-/*<section>
-<div class="sessao-orcamento">
-    <div class="orgamento-wrapper">
-        <h1>Faça um orçamento</h1>
-        <form>
-            <label for="qtde">Qtde de Páginas</label>
-            <input type="text" name="qtde" id="qtde">
-            <label for="js">Preciso de um script JS</label>
-            <input type="checkbox" name="js" id="js">
-            <label>Layout</label>
-            <div class="group-layout">
-                <div>
-                    <input type="radio" name="layout"  id="layout-sim">
-                    <label for="layout-sim">Preciso de umm layout</label>
-                </div>
-                <div>
-                    <input type="radio" name="layout" id="layout-nao">
-                    <label for="layout-nao">Já tenho um layout</label>
-                </div>
-            </div>
-            <label for="prazo">Prazo</label>
-            <input type="range" name="prazo" id="prazo">
-            <button class="btn btn-orcamento" type="submit">Enviar</button>
-        </form>
-    </div>
-</div>
-</section>*/ 
 
+    function atualizarPreco(){
+
+        const qtde = document.querySelector("#qtde").value;
+        let preco = qtde * 100;
+
+        const temJS = document.querySelector("#js").checked;
+        if (temJS) preco *= 1.1;
+
+        const incluiLayout = document.querySelector("#layout-sim").checked;
+        if (incluiLayout) preco += 400;
+
+        const prazo = document.querySelector("#prazo").value;
+        let taxaUrgencia = 1 - prazo*0.1;
+        preco *= 1 + taxaUrgencia;
+
+        document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`;
+    }
